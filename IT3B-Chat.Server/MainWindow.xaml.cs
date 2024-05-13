f897125e95fecfe738e67faf3c7fab638acecb73
Adam Sivera <Sivera.Adam@stud.vassboskovice.cz>
2024-05-13 09:22:31 +0200

    2.

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IT3B_Chat.Server
 {
- /// <summary>
- /// Interaction logic for MainWindow.xaml
- /// </summary>
- public partial class MainWindow : Window
- {
-  public MainWindow()
-  {
-   InitializeComponent();
-  }
- }
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> Messages { get; set; }
+        public ObservableCollection<string> ConnectionEvents { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Messages = new ObservableCollection<string>();
+            ConnectionEvents = new ObservableCollection<string>();
+
+            // Příklad přidání několika zpráv a událostí připojení/odpojení klientů (pro účely demonstrace)
+            Messages.Add("Nová zpráva od klienta A: Dobrý den!");
+            Messages.Add("Nová zpráva od klienta B: Ahoj, jak se máte?");
+            ConnectionEvents.Add("Nový klient připojen: 192.168.0.1");
+            ConnectionEvents.Add("Klient odpojen: 192.168.0.2");
+        }
+    }
 }
